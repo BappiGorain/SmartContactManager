5f97c552635fa29dfecda273000c624b24c240e9
BappiGorain <bappigorain08312@gmail.com>
2025-07-19 23:01:42 +0530

    view page  added

---
+++ src/main/resources/static/js/contacts.js
@@ -0,0 +1,82 @@
+console.log("contact.js");
+
+
+const viewContactModal = document.getElementById("view_contact_modal");
+
+
+// options with default values
+const options = {
+    placement: 'bottom-right',
+    backdrop: 'dynamic',
+    backdropClasses:
+        'bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40',
+    closable: true,
+    onHide: () => {
+        console.log('modal is hidden');
+    },
+    onShow: () => {
+        console.log('modal is shown');
+    },
+    onToggle: () => {
+        console.log('modal has been toggled');
+    },
+};
+
+// instance options object
+const instanceOptions = {
+  id: 'view_contact_modal',
+  override: true
+};
+
+
+const contactModal = new Modal(viewContactModal,options,instanceOptions);
+
+function openContactModal()
+{
+    contactModal.show();
+}
+
+function closeContactModal()
+{
+    contactModal.hide();
+}
+
+
+async function loadContactdata(id)
+{
+    console.log(id);
+
+    try
+    {
+        const data = await(
+            await fetch(`http://localhost:8080/api/contacts/${id}`)
+        ).json();
+        console.log(data);
+
+        document.querySelector("#contact_name").innerHTML = data.name;
+        document.querySelector("#contact_email").innerHTML = data.email;
+        document.querySelector("#contact_phoneNumber").innerHTML = data.phoneNumber;
+        document.querySelector("#contact_address").innerHTML = data.address;
+        // document.querySelector("#contact_picture").innerHTML = data.picture;
+        document.querySelector("#contact_description").innerHTML = data.description;
+        document.querySelector("#contact_favorite").innerHTML = data.favorite;
+        document.querySelector("#contact_websiteLink").innerHTML = data.websiteLink;
+        document.querySelector("#contact_linkedInLink").innerHTML = data.linkedInLink;
+
+        document.querySelector("#contact_websiteLink").href = data.websiteLink;
+        document.querySelector("#contact_linkedInLink").href = data.linkedInLink;
+
+        
+        
+        
+        
+        
+        
+        openContactModal();
+
+        
+    } catch(error)
+    {
+        console.log("Error : ",error);
+    }
+}
